6956f41eca7930b03037c45e8527fa18960f3405
sonuarman <mansooriarman090@gmail.com>
2024-04-16 18:33:44 +0530

    nextupdate

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import ThemeToggler from "./theme-toggler";
+import { Button } from "./ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
-export default function Navbar() {
+
+const GITHUB_URL = "https://github.com/atharvadeosthale/hashnode-headless-blog";
+
+export default async function Navbar() {
+ 
+
   return (
-    <div>
-      hello
+    <div className="w-full border-b mb-5">
+      <div className="max-w-7xl w-full px-3 xl:p-0 my-5 mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">
+          Arman.
+        </div>
+        <div className="flex items-center gap-5">
+          <ThemeToggler />
+
+          <Button variant="secondary">
+            <Link
+              className="gap-2 flex items-center"
+              href={GITHUB_URL}
+              target="_blank"
+            >
+              <GitHubLogoIcon /> GitHub
+            </Link>
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
