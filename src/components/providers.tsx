43810e03a5449fd972745d624875d7a5cc91b9b3
sonuarman <mansooriarman090@gmail.com>
2024-04-16 12:54:14 +0530

    provider

---
+++ src/components/providers.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "./theme-provider";
+import React, { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
