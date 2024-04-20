170627526a5be460196cd758dfe0bb482b9ecbfa
sonuarman <mansooriarman090@gmail.com>
2024-04-20 14:07:26 +0530

    posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
- <main>
-  
- </main>
+    <main className="max-w-7xl w-full px-3 xl:p-0 mx-auto">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-5">
+      
+    </div>
+  </main>
    
   );
 }
